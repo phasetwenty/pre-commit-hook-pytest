d0b454134c5e000af912f0666bde38b1be49a176
Christopher Haverman <phasetwenty@gmail.com>
2017-03-20 09:49:21 -0700

    Switching the invocation to be testing.

--- run-pytest.sh
+++ run-pytest.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-if ! type py.test 2>/dev/null; then
+if ! type py.test >/dev/null 2>&1; then
     >&2 echo "pytest not found on your PATH. Perhaps you need to activate your virtualenv?"
     exit 1
 fi
 
-py.test "$@"
+echo "py.test $@"
+exit 1
